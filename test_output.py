45cca8f29e78bd1b52653503f2abbb20a3e98b4e
Mandy <aisenor@upei.ca>
2021-01-28 15:11:17 -0400

    Updated tests

--- test_output.py
+++ test_output.py
@@ -12,17 +12,18 @@ import run # module
 # start_date = '2020-01-12'
 # end_date = '2020-01-19'
 
-def test_output():
+def test_accept_input():
     run.input = lambda x: '2020-01-12'
-    output = run.accept_input('start date')
-    assert output == '2020-01-12'
+    output_x = run.accept_input('start date')
+    assert output_x == '2020-01-12'
 
     run.input = lambda y: '2020-01-19'
-    output = run.accept_input('end date')
-    assert output == '2020-01-19'
-    # output = run
-    # runpy.run_path(script)
-    # accept_input.input = lambda: '2020-01-01'
-    # output = accept_input('start date')
-    # assert output == '2020-01-01'
-    assert 1==1
+    output_y = run.accept_input('end date')
+    assert output_y == '2020-01-19'
+
+def test_team_rank(capfd):
+    run.input = lambda y: '2020-01-19'
+    output = run.main()
+    out, err = capfd.readouterr()
+    assert "event_id" in out
+    assert "home_rank_points" in out
